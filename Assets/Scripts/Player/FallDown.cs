8252b706cdf8608212babdf8a9934a611c3d59a8
Jooglorystar <121430174+Jooglorystar@users.noreply.github.com>
2024-10-25 14:46:04 +0900

    Feat: Apply fall down system
    플레이어가 떨어지면 처음 위치로 이동

---
+++ Assets/Scripts/Player/FallDown.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FallDown : MonoBehaviour
+{
+    private Vector3 startPoint;
+
+    private void Start()
+    {
+        startPoint = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("GameController"))
+        {
+            transform.position = startPoint;
+            transform.rotation = Quaternion.Euler(Vector3.zero);
+        }
+    }
+}
