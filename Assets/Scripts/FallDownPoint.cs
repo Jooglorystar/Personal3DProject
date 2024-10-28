ba5c01bb7dc7fa377c040a4ef9d980c0dddd90e4
Jooglorystar <121430174+Jooglorystar@users.noreply.github.com>
2024-10-28 10:17:59 +0900

    Feat: Apply Item fall down

---
+++ Assets/Scripts/FallDownPoint.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDownPoint : MonoBehaviour
+{
+    private Vector3 startPoint;
+
+    private void Start()
+    {
+        startPoint = PlayerManager.Instance.Player.gameObject.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Player":
+                other.gameObject.transform.position = startPoint;
+                other.gameObject.transform.rotation = Quaternion.Euler(Vector3.zero);
+                PlayerManager.Instance.Player.condition.TakePhysicalDamage(50);
+                break;
+            case "Item":
+                Destroy(other.gameObject);
+                break;
+        }
+    }
+}
